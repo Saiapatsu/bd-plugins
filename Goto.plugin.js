4203210ebcd1f48a41f50a6a3c98f47a8d4f6750
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2021-10-05 10:13:32 +0300

    Check in Goto

---
+++ Goto.plugin.js
@@ -0,0 +1,40 @@
+/**
+ * @name Goto
+ * @source https://github.com/Saiapatsu/bd-plugins/raw/master/Goto.plugin.js
+ */
+
+const {readText} = require("electron").clipboard;
+const {transitionTo} = BdApi.findModuleByProps('transitionTo'); // this gets the navigator module, which contains transitionTo
+const {showToast} = BdApi;
+
+module.exports = class Goto {
+	getName        =()=> "Goto"
+	getDescription =()=> "Press Ctrl-G to navigate to the message link in the clipboard"
+	getVersion     =()=> "0"
+	getAuthor      =()=> "Wist"
+	
+	load  =()=> {}
+	start =()=> document.body.   addEventListener("keydown", this.listener, true);
+	stop  =()=> document.body.removeEventListener("keydown", this.listener, true);
+	
+	listener =e=> {
+		if (
+			e.keyCode  == 71    & // g
+			e.ctrlKey  == true  &
+			e.shiftKey == false &
+			e.altKey   == false
+		) {
+			e.preventDefault();
+			e.stopImmediatePropagation();
+			const text = readText()
+			const [, thisserver, thischannel] = document.location.pathname.match(/\/channels\/(\d+)\/(\d+)/) || [];
+			const [, server, channel, message] = text.match(/(\d*?)\/?(\d*?)\/?(\d*)$/);
+			if (!(server || thisserver) || !(channel || thischannel)) {
+				showToast("Nope", {type: "warning"});
+				return;
+			}
+			showToast(text);
+			transitionTo(`/channels/${server || thisserver}/${channel || thischannel}/${message || 0}`);
+		}
+	}
+}
