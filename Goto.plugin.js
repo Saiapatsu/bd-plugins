da480e579e5d34c8319e5a22c7e19780299b684b
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2025-05-29 23:03:10 +0300

    Goto: get stores by name, cleanup, add *guild/#channel decoders

--- Goto.plugin.js
+++ Goto.plugin.js
@@ -7,20 +7,17 @@
  */
 
 const readClipboard = DiscordNative.clipboard.read;
-// const {transitionTo} = BdApi.findModuleByProps("transitionTo"); // this gets the navigator module, which contains transitionTo
-// temporary measure
-// const transitionTo = BdApi.Webpack.getModule((exports, module, index) => module.exports.uL && module.exports.DB).uL;
-// BdApi.Webpack.getModule((exports, module, index) => index === "655695")
 const [
-	{getChannel, hasChannel},
-	{uL: transitionTo},
-	UnreadsStore,
+	{uL: transitionTo}, // Navigator
+	{getChannel, hasChannel, getChannelIds},
+	{getGuild},
+	{getReadStatesByChannel},
 ] = BdApi.Webpack.getBulk(...[
-	["getChannel", "hasChannel"],
-	// ["transitionTo", "transitionToGuild", "back", "forward", "getHistory"],
-	["At", "DB", "DR", "Wf", "XU", "dL", "eH", "m1", "op", "s1", "uL", "uv"],
-	["getReadStatesByChannel", "getMentionCount"],
-].map(x => ({filter: BdApi.Webpack.Filters.byProps(...x)})));
+	BdApi.Webpack.Filters.byProps("At", "DB", "DR", "Wf", "XU", "dL", "eH", "m1", "op", "s1", "uL", "uv"), // ["transitionTo", "transitionToGuild", "back", "forward", "getHistory"],
+	BdApi.Webpack.Filters.byStoreName("ChannelStore"), // ["getChannel", "hasChannel"],
+	BdApi.Webpack.Filters.byStoreName("GuildStore"),
+	BdApi.Webpack.Filters.byStoreName("ReadStateStore"), // ["getReadStatesByChannel", "getMentionCount"],
+].map(x => ({filter: x})));
 
 /*
 copy(Object.entries(BdApi.Webpack.getByKeys("At", "DB", "DR", "Wf", "XU", "dL", "eH", "m1", "op", "s1", "uL", "uv")).map(([k, v]) => k + ": " + v.toString()).join("\n\n"))
@@ -76,7 +73,7 @@ function regexToSnowflake(arr) {
 	return msecToSnowflake(new Date(...arr).getTime());
 }
 
-// a decoder returns either false, empty array or a string[] message, channel?, server?
+// A decoder errors or returns false or string[message?, channel?, server?]
 const decoders = [
 	// titled link
 	str => {
@@ -85,39 +82,45 @@ const decoders = [
 		if (params.get("origin") !== "discord") return;
 		return [params.get("messageid"), params.get("channelid"), params.get("serverid")];
 	},
+	// *server, arbitrary channel
+	str => [null, null, getGuild(str.match(/^\*(\d+)/)[1]).id],
+	// #channel
+	str => [null, getChannel(str.match(/^#(\d+)/)[1]).id],
 	// attachment link
 	str => str.match(/\/attachments\/(\d+)\/(\d+)\//).slice(1).reverse(),
 	// YYYYMMDDHHMMSS
-	str => regexToSnowflake(/^\s*(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)\s*$/.exec(str)),
+	str => [regexToSnowflake(/^\s*(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)\s*$/.exec(str))],
 	// YYYY-MM-DD HH:MM:SS
 	// str => regexToSnowflake(/^\s*(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)\s*$/.exec(str)),
-	str => regexToSnowflake(/^\s*(\d\d\d\d)[-_ ]?(\d\d)[-_ ]?(\d\d)[-_ ]?(\d\d)[-_: ]?(\d\d)[-_: ]?(\d\d)\s*$/.exec(str)),
+	str => [regexToSnowflake(/^\s*(\d\d\d\d)[-_ ]?(\d\d)[-_ ]?(\d\d)[-_ ]?(\d\d)[-_: ]?(\d\d)[-_: ]?(\d\d)\s*$/.exec(str))],
 	// Pair of channel and message separated by space, needed this once
 	str => str.match(/^\s*(\d+) (\d+)/).slice(1).reverse().filter(Boolean),
 	// Unix seconds or milliseconds timestamp between Discord epoch and now
 	// collision with snowflakes won't be a concern within my lifetime
 	str => {
 		str = Number(/\s*(\d+)\s*/.exec(str)[1]);
-		return str
+		return [str
 			&& (str >= 1420070400    && str < Date.now() / 1000) ? msecToSnowflake(str * 1000)
-			:  (str >= 1420070400000 && str < Date.now()        && msecToSnowflake(str       ))
+			:  (str >= 1420070400000 && str < Date.now()        && msecToSnowflake(str       ))]
 	},
 	// discord message url or just message id, tolerating a comment after a space
 	str => str.match(/\S*/)[0].match(/(@me|\d*?)\/?(\d*?)\/?(\d*)$/).slice(1).reverse().filter(Boolean),
 ];
 
 // returns false or array
-function verify(value) {
-	return value && (typeof value == "object" ? (value.length && value) : [value]);
-}
+// function verify(value) {
+	// // return value && (typeof value == "object" ? (value.length && value) : [value]);
+	// return value;
+// }
 
 // returns array [message, channel, server]
 function decode(str) {
 	let value;
 	for (var f of decoders)
 		try {
-			if (value = verify(f(str)))
-				return value;
+			return f(str);
+			// if (value = verify(f(str)))
+				// return value;
 		} catch (e) {}
 	return [];
 }
@@ -127,15 +130,27 @@ function listener(e) {
 		// Go to message in clipboard
 		e.preventDefault();
 		e.stopImmediatePropagation();
+		
 		const str = readClipboard().trim();
+		
 		const [, thisserver, thischannel] = document.location.pathname.match(/\/channels\/([^\/]+)\/([^\/]+)/) || [];
+		
 		let [message, channel, server] = decode(str);
-		if (!message) return BdApi.UI.showToast("Incomprehensible\n" + str, {type: "warning"});
-		if (!Number(server) && channel) server = getChannel(channel)?.guild_id || server;
-		channel = channel || thischannel;
-		server = server || thisserver;
-		if (!server || !channel) return BdApi.UI.showToast("Cannot resolve\n" + str, {type: "warning"});
-		if (server !== "@me" && !hasChannel(channel)) {
+		
+		if (message === undefined)
+			return BdApi.UI.showToast("Incomprehensible\n" + str, {type: "warning"});
+		
+		if (message)
+			channel = channel || thischannel;
+		
+		if (channel)
+			server = getChannel(channel)?.guild_id || server || thisserver;
+		
+		if (server && !channel) {
+			channel = getChannelIds(server)[0];
+		}
+		
+		if (server !== "@me" && channel && !hasChannel(channel)) {
 			const rope = [
 				"server " + server,
 				"channel " + channel,
@@ -144,8 +159,9 @@ function listener(e) {
 			];
 			return BdApi.UI.showToast("Unknown channel\n" + rope.join("\n"), {type: "warning"});
 		}
+		
 		BdApi.UI.showToast(str);
-		transitionTo(`/channels/${server}/${channel}/${message}`);
+		transitionTo("/" + ["channels", server, channel, message].filter(Boolean).join("/"));
 		
 	} else if (e.keyCode == 33 && e.ctrlKey && e.shiftKey && !e.altKey) { // Ctrl+Shift+PageUp
 		// Go to beginning of channel
@@ -166,7 +182,7 @@ function listener(e) {
 		const [, server, channel] = document.location.pathname.match(/\/channels\/([^\/]+)\/([^\/]+)/) || [];
 		if (!channel) return BdApi.UI.showToast("Could not identify current channel", {type: "warning"});
 		
-		const state = UnreadsStore.getReadStatesByChannel()[channel];
+		const state = getReadStatesByChannel()[channel];
 		const message = state?.ackMessageId;
 		if (message) {
 			transitionTo(`/channels/${server}/${channel}/${message}`);
